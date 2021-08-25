097b2df77f0581d60c7cda67ac20688a39a19b60
SprayShadow <89224810+SprayShadow@users.noreply.github.com>
2021-08-26 02:33:44 +0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-username ="username"                                #your reddit username
-password = "password"                               #your reddit password
-client_id = "personal use script"                   #your personal use script
-client_secret = "secret"                            #your secret
