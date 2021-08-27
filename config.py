8f7b63aa06d0b74826d51ea19c95fe9598058237
SprayShadow <89224810+SprayShadow@users.noreply.github.com>
2021-08-27 23:25:55 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+username ="username"                                #your reddit username
+password = "password"                               #your reddit password
+client_id = "personal use script"                   #your personal use script
+client_secret = "secret"                            #your secret
