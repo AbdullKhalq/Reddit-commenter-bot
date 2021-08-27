a51538bdf6e0c6f3161698ff08dd116ce701ede9
SprayShadow <89224810+SprayShadow@users.noreply.github.com>
2021-08-27 23:25:28 +0300

    Delete Reddit commenter bot directory

--- Reddit commenter bot/config.py
+++
@@ -1,4 +0,0 @@
-username ="username"                                #your Reddit username
-password = "password"                               #your Reddit password
-client_id = "personal use script"                   #your personal use script
-client_secret = "secret"                            #your secret
